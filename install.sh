741024c3a5b0b099f2a33c4ca50cb362021fcf78
FlamerMC <153970526+FlamerMC@users.noreply.github.com>
2024-08-05 05:25:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
+echo Getting The Zip file
+sleep 1
+echo Running: wget https://github.com/FlamerMC/flame-tv-eletron/releases/download/0.0.1/flametv-linux-x64.zip
 wget https://github.com/FlamerMC/flame-tv-eletron/releases/download/0.0.1/flametv-linux-x64.zip
+echo Extracting
+sleep 1
+echo Running unzip flametv-linux-x64.zip
 unzip flametv-linux-x64.zip
+echo Going into: flametv-linux-x64
 cd flametv-linux-x64
+echo Opening App!!!!
+sleep 1
 bash ./flametv
